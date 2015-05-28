d7fe4afeb719cfb5a40f42216992f7aece3079ed
lynneeai <lynneeai@hotmail.com>
2015-05-28 15:08:56 -0400

    added authenticate classes

---
+++ InterceptorTester/API Operations/Authenticate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Authenticate : APIOperation
+	{
+		public Authenticate(Uri server, string serialNum)
+		{
+			opHost = server;
+			hOp = HTTPOperation.GET;
+			opQuery = new HTTPQuery(QueryParameter.i, serialNum);
+		}
+
+		public override string getExpectedResult()
+		{
+			if (opQuery.isValid())
+			{
+				return "200";
+			}
+			else
+			{
+				return "400";
+			}
+		}
+
+		public override string ToString()
+		{
+			return "Authenticate";
+		}
+
+		public override Uri getUri()
+		{
+			return new Uri(opHost, "/api/Authenticate/" + opQuery.ToString());
+		}
+
+		public override object getJson()
+		{
+			return null;
+		}
+	}
+}
+
+
---
+++ InterceptorTester/JSON/AuthenticateJSON.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConsoleApplication1
+{
+
+	//[DataContract]
+
+	public class AuthenticateJSON
+	{
+
+		public bool isValid ()
+		{
+			if ((userID != null) && (password != null))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		string userID;
+
+		string password;
+
+		// ReSharper restore InconsistentNaming
+
+		public override string ToString()
+		{
+		}
+	}
+
+
+
+}
