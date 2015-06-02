7d030910ed30005f189e7ac654af6207ceee75a4
kleril <kleril@users.noreply.github.com>
2015-06-02 14:40:32 -0400

    Added scan loops

--- DatabasePopulator/Program.cs
+++ DatabasePopulator/Program.cs
@@ -13,20 +13,78 @@ namespace DatabasePopulator
             generateScans();
         }
 
-        private static void generateScans()
+        private static async void generateScans()
         {
             int[] pseudoRandDelay = new int[100];
-            int[] pseudoRandRepeat = new int[100];
+            int[] pseudoRandBasket = new int[100];
+            ConsoleApplication1.HTTPSCalls caller = new ConsoleApplication1.HTTPSCalls();
 
             for (int i = 0; i < 100; i++)
             {
-                System.Threading.Thread.Sleep(pseudoRandDelay[i]);
+                System.Threading.Thread.Sleep(pseudoRandDelay[i] * 10);
+                for (int j = 0; j < pseudoRandBasket[i]; j++)
+                {
+                    System.Threading.Thread.Sleep(pseudoRandDelay[i]);
+                    foreach (ConsoleApplication1.Test nextScan in getBasket(j))
+                    {
+                        await caller.runTest(nextScan, ConsoleApplication1.HTTPOperation.POST);
+                    }
+                }
+            }
+        }
 
-                scan = ConsoleApplication1.dummyScans.getScan(scanEnum);
+        private static List<ConsoleApplication1.Test> getBasket(int basketType)
+        {
+            List<ConsoleApplication1.Test> basket;
+
+            switch (basketType)
+            {
+                case 1:
+                    basket.Add(ConsoleApplication1.getScan(3));
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(4));
+                    break;
+                case 2:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(4));
+                case 3:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(1));
+                case 4:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                case 5:
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(4));
+                case 6:
+                    basket.Add(ConsoleApplication1.getScan(3));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                case 7:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(3));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                case 8:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                case 9:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(4));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                case 10:
+                    basket.Add(ConsoleApplication1.getScan(1));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(2));
+                    basket.Add(ConsoleApplication1.getScan(4));
             }
 
-            ConsoleApplication1.HTTPSCalls calls = new ConsoleApplication1.HTTPSCalls();
-            calls.runTest()
+            return basket;
         }
     }
 }
--- InterceptorTester/Test.cs
+++ InterceptorTester/Test.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Test
+    public class Test
     {
         APIOperation operation;
         String expectedResult;
