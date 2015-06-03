5fd847694a78df49a6d9bc8825b6d2ce9933c061
kleril <kleril@users.noreply.github.com>
2015-06-03 12:15:44 -0400

    basket tweaks

--- DatabasePopulator/Program.cs
+++ DatabasePopulator/Program.cs
@@ -21,7 +21,7 @@ namespace DatabasePopulator
         private static async void generateScans()
         {
             int[] pseudoRandDelay = {12,44,52,122,55,93,21,62,49,25,25,65,83,12,24,66,12,12,182,75,76,712,42,120};
-            int[] pseudoRandBasket = {10,4,2,4,5,6,7,8,9,1,5,4,2,1,8,9,7,6,4,5,3,3,5,9,8,7,6,5,4,3,1,2,6,2,2,1,2,2,4,2};
+            int[] pseudoRandBasket = {10,4,2,4,5,6,7,8,9,1,5,4,2,1,8,9,7,6,4,10,3,3,5,9,8,1,7,6,5,4,3,10,2,6,2,2,1,2,2,4,2};
 
             for (int i = 0; i < pseudoRandDelay.Length; i++)
             {
@@ -30,7 +30,7 @@ namespace DatabasePopulator
                 for (int j = 0; j < pseudoRandBasket.Length; j++)
                 {
                     Console.WriteLine("Basket complete. Sleeping...");
-                    System.Threading.Thread.Sleep(pseudoRandDelay[i]);
+                    System.Threading.Thread.Sleep(pseudoRandDelay[i] * 10);
                     Console.WriteLine("Getting next basket");
                     getBasket(pseudoRandBasket[j]);
                     foreach (ConsoleApplication1.Test nextScan in basket)
