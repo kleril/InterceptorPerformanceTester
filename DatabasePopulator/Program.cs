3e8b6bc1a042fd716346ba4b5ea58d287c6c4266
lynneeai <lynneeai@hotmail.com>
2015-06-05 09:19:11 -0400

    ..

--- DatabasePopulator/Program.cs
+++ DatabasePopulator/Program.cs
@@ -20,8 +20,8 @@ namespace DatabasePopulator
 
         private static async void generateScans()
         {
-			int[] pseudoRandDelay = {60};//, 120, 600};
-			int[] pseudoRandBasket = {10,4};//,2,4,5,6,7,8,9,1,5,4,2,1,8,9,7,6,4,10,3,3,5,9,8,1,7,6,5,4,3,10,2,6,2,2,1,2,2,4,2};
+			int[] pseudoRandDelay = {60, 120, 600, 60, 120, 480, 300, 180};
+			int[] pseudoRandBasket = {10,4,2,4,5,6,7,8,9,1,5,4,2,1,8,9,7,6,4,10,3,3,5,9,8,1,7,6,5,4,3,10,2,6,2,2,1,2,2,4,2};
 
 			foreach (int delay in pseudoRandDelay)
             {
